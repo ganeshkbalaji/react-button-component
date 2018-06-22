4835e291c4a7ce2cbd0d7f979c79edc1fd5fb274
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-06-21 21:53:45 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class Button extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {counter: 0};
+    }
+
+    handleClick = () => {
+        this.setState({
+            counter: this.state.counter+=1
+        })
+    };
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>{this.state.counter}</button>
+        )
+    }
+    
+}
+
+
+ReactDOM.render(<Button />, document.getElementById('root'));
