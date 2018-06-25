452d0a6dd81ecd91e5081dfe8b263158af8624bc
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-06-24 22:34:04 -0700

    added new components

--- src/index.js
+++ src/index.js
@@ -3,19 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Button extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {counter: 0};
+    handleClick = () => {
+        this.props.onClickFunction(this.props.incrementValue);
     }
-    handleClick = ()  => {
-        this.setState({
-            counter: this.state.counter +=1
-        })
-    };
     render() {
     return (
-        <button onClick={this.handleClick}>{this.state.counter}</button>
+        <button onClick={this.handleClick}>+{this.props.incrementValue}</button>
     )
     }
 }
-ReactDOM.render(<Button />, document.getElementById('root'));
+
+const Result =  (props) => {
+    return (
+        <div>{props.counter}</div>
+    )
+}
+
+class App extends React.Component {
+    state = {counter: 0};
+
+    incrementCounter = (incrementValue) => {
+        this.setState((prevState) =>({
+            counter: prevState.counter + incrementValue
+        }));
+    };
+    render() {
+        return (
+            <div>
+                <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+                <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+                <Result counter={this.state.counter} />
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
